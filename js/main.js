8070d19b7b1e712410b6828aa0f54e6e002f1521
tpayne52 <90876397+tpayne52@users.noreply.github.com>
2023-04-18 18:43:48 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.querySelector("#home").addEventListener("click", function() {
+    window.location.href = "index.html";
+});
+
+document.querySelector("#git").addEventListener("click", function() {
+    window.location.href = "https://github.com/tpayne52";
+});
+
+document.querySelector("#linkedin").addEventListener("click", function() {
+    window.location.href = "https://www.linkedin.com/in/thomas-b-payne/";
+});
